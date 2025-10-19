3adc93b2e8b0813ada4cce5afbebc015765a5a30
Pablo Hernández <jhernandezg41@miumg.edu.gt>
2025-10-19 00:06:08 -0600

    Fix: eliminar BrowserRouter duplicado en App.tsx

--- retrogaming-frontend/src/App.tsx
+++ retrogaming-frontend/src/App.tsx
@@ -1,21 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Checkout from './pages/Checkout';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/checkout" element={
-          <ProtectedRoute>
-            <Checkout />
-          </ProtectedRoute>
-        } />
-        {/* otras rutas */}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/checkout" element={
+        <ProtectedRoute>
+          <Checkout />
+        </ProtectedRoute>
+      } />
+      {/* otras rutas */}
+    </Routes>
   );
 }
 
