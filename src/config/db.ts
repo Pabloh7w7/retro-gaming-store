f956e983fa88c769bdf01399ca0c323518fafbfe
Pablo Hernández <jhernandezg41@miumg.edu.gt>
2025-10-23 21:34:14 -0600

    Mooooaaaarrr x100

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import mariadb from 'mariadb';
 
 export const pool = mariadb.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: '12345',
   database: 'retrogaming',
